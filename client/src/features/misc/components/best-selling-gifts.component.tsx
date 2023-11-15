de402a9ed06d4f97bde461a9dee87f6c2a2d8e73
FadyAmir223 <fadyamir223@gmail.com>
2023-11-16 00:04:47 +0200

    best selling gifts

--- client/src/features/misc/components/best-selling-gifts.component.tsx
+++ client/src/features/misc/components/best-selling-gifts.component.tsx
@@ -1,100 +1,38 @@
 import { useState } from 'react';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 import { cn } from '@/utils/cn';
-import LinkCustom from '@/components/link-custom.component';
-import heroLinks from '../data/men-women-url.json';
-
-const gifts = [
-  {
-    text: 'for everyday',
-    items: [
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-WR2-Carousel-Card-1110x1110.webp',
-        miniImgUrl: '/images/main-page/mini-art/Weather.webp',
-        title: 'wool runner 2',
-        description: 'Step Into Next-Gen Comfort',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-2.webp',
-        miniImgUrl: '/images/main-page/mini-art/Weather.webp',
-        title: 'wool runner-up mizzle',
-        description: 'wet weather high top',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-1 (2).webp',
-        miniImgUrl: '/images/main-page/mini-art/Tree.webp',
-        title: 'tree runner',
-        description: 'breezy, everyday sneaker',
-      },
-    ],
-  },
-  {
-    text: 'for travel',
-    items: [
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-1 (1).webp',
-        miniImgUrl: '/images/main-page/mini-art/Tree.webp',
-        title: 'tree dasher 2',
-        description: 'comfy, breezy, everyday runs',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-2 (1).webp',
-        miniImgUrl: '/images/main-page/mini-art/Weather.webp',
-        title: 'wool dasher mizzle',
-        description: 'comfy, water-repellent, everyday runs',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-3.webp',
-        miniImgUrl: '/images/main-page/mini-art/Tree.webp',
-        title: 'tree flyer 2',
-        description: 'comfortable, springy active shoe',
-      },
-    ],
-  },
-  {
-    text: 'for lounging',
-    items: [
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-1.webp',
-        miniImgUrl: '/images/main-page/mini-art/Weather.webp',
-        title: 'wool dweller',
-        description: 'cozy slipper',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-4.webp',
-        miniImgUrl: '/images/main-page/mini-art/Weather.webp',
-        title: 'wool lounger',
-        description: 'cozy slip-on',
-      },
-      {
-        imgUrl:
-          '/images/main-page/favorites/23Q4-Holiday_Ch1-Site-Category_Category_Card-1110x1110-3 (2).webp',
-        miniImgUrl: '/images/main-page/mini-art/fluff_pdp_icon.webp',
-        title: 'wool lounger fluff',
-        description: 'cozy feel, fluffy material',
-      },
-    ],
-  },
-];
+import GiftCard from './gift-card.component';
+import gifts from '../data/gifts.json';
+import screenSize from '@/data/screen-size.json';
 
 const BestSellingGifts = () => {
-  const [currGift, setCurrGift] = useState(0);
+  const [gift, setGift] = useState({
+    section: 0,
+    card: 0,
+    total: gifts.flatMap((figt) => figt.items).length,
+  });
 
-  const handleGiftChange = (idx: number) => {
-    setCurrGift(idx);
+  const imgSize = (innerWidth - 8 * 2) / (screenSize.sm > innerWidth ? 1 : 2);
+
+  const handleGiftSection = (idx: number) => {
+    setGift((prevGift) => ({
+      ...prevGift,
+      section: idx,
+      card: idx * 3,
+    }));
+  };
+
+  const handleGiftChange = (direction: 1 | -1) => {
+    setGift((prevGift) => ({
+      ...prevGift,
+      card: prevGift.card + direction,
+    }));
   };
 
   return (
     <section className='px-6'>
-      <div className='px-9'>
+      <div className='lg:px-9'>
         <h2 className='capitalize font-bold text-[26px] text-center mb-2'>
           our best-selling gifts
         </h2>
@@ -103,15 +41,16 @@ const BestSellingGifts = () => {
           <div className='flex justify-center relative'>
             <span
               className='absolute -bottom-[2px] w-44 bg-black left-0 h-[2px] duration-150'
-              style={{ translate: `${100 * currGift}%` }}
+              style={{ translate: `${100 * gift.section}%` }}
             />
 
             {gifts.map((gift, idx) => (
               <button
+                key={gift.text}
                 className={cn(
                   'allbirds-font p-3 border-b-2 border-b-transparent w-44 tracking-[1px]',
                 )}
-                onClick={() => handleGiftChange(idx)}
+                onClick={() => handleGiftSection(idx)}
               >
                 {gift.text}
               </button>
@@ -119,47 +58,53 @@ const BestSellingGifts = () => {
           </div>
         </div>
 
-        <div className='flex gap-x-5 justify-center'>
-          {gifts[currGift].items.map((giftItem) => (
-            <div
-              key={giftItem.title}
-              className='group shadow-lg shadow-gray overflow-hidden group w-1/3'
-            >
-              <div className='relative group/overlay w-full pb-[100%] overflow-hidden'>
-                <img
-                  src={giftItem.imgUrl}
-                  alt=''
-                  className='absolute top-0 left-0 w-full h-full object-cover duration-500 transition-transform group-hover:scale-[105%]'
-                />
-                <div className='absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-out bg-gray opacity-0 group-hover/overlay:opacity-40'></div>
-
-                <div className='absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-out opacity-0 group-hover/overlay:opacity-100 flex justify-center items-center'>
-                  <div className='flex flex-col gap-y-3'>
-                    {heroLinks.map((heroLink) => (
-                      <LinkCustom
-                        key={heroLink.text}
-                        to={heroLink.url}
-                        className='z-30 py-[10px] bg-white hover:bg-gray border-none duration-100 block relative w-full'
-                      >
-                        {heroLink.text}
-                      </LinkCustom>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className='px-6 py-3 text-gray'>
-                <h4 className='capitalize border-b-[1px] border-b-gray-light font-bold pb-[10px] mb-[10px] text-[14px] tracking-wide'>
-                  {giftItem.title}
-                </h4>
-                <div className='flex gap-x-2 items-center'>
-                  <img src={giftItem.miniImgUrl} alt='' className='w-6' />
-                  <p className='leading-[1.4] text-[12px] tracking-[0.3px]'>
-                    {giftItem.description}
-                  </p>
-                </div>
-              </div>
-            </div>
+        <div className='hidden md:flex gap-x-2 justify-center'>
+          {gifts[gift.section].items.map((giftItem) => (
+            <GiftCard key={giftItem.title} {...giftItem} className='w-full' />
           ))}
+        </div>
+
+        <div className=' md:hidden relative'>
+          <div
+            className='whitespace-nowrap flex gap-x-2 duration-500 '
+            style={{ translate: `-${gift.card * (imgSize + 8)}px` }}
+          >
+            {gifts.map((gift) =>
+              gift.items.map((giftItem) => (
+                <GiftCard
+                  key={giftItem.title}
+                  {...giftItem}
+                  style={{ minWidth: imgSize }}
+                />
+              )),
+            )}
+          </div>
+
+          <button
+            className={cn(
+              'absolute right-0 top-1/2 -translate-y-1/2 rounded-full bg-gray text-gray-light grid place-items-center w-10 h-10',
+              { hidden: gift.card > gift.total - 3 },
+            )}
+            onClick={() => handleGiftChange(1)}
+            disabled={gift.card > gift.total - 3}
+          >
+            <span className='scale-150'>
+              <FaAngleRight />
+            </span>
+          </button>
+
+          <button
+            className={cn(
+              'absolute left-0 top-1/2 -translate-y-1/2 rounded-full bg-gray text-gray-light grid place-items-center w-10 h-10',
+              { hidden: gift.card === 0 },
+            )}
+            onClick={() => handleGiftChange(-1)}
+            disabled={gift.card === 0}
+          >
+            <span className='scale-150'>
+              <FaAngleLeft />
+            </span>
+          </button>
         </div>
       </div>
     </section>
---
+++ client/src/features/misc/components/gift-card.component.tsx
@@ -0,0 +1,79 @@
+import { HTMLAttributes } from 'react';
+
+import LinkCustom from '@/components/link-custom.component';
+import heroLinks from '../data/men-women-url.json';
+import { cn } from '@/utils/cn';
+
+type GiftCardProps = HTMLAttributes<HTMLDivElement> & {
+  title: string;
+  miniImgUrl: string;
+  imgUrl: string;
+  description: string;
+};
+
+const GiftCard = ({
+  imgUrl,
+  miniImgUrl,
+  title,
+  description,
+  className,
+  ...props
+}: GiftCardProps) => {
+  return (
+    <div
+      className={cn(
+        'group shadow-lg shadow-gray overflow-hidden group',
+        className,
+      )}
+      {...props}
+    >
+      <div className='relative group/overlay w-full pb-[100%] overflow-hidden'>
+        <img
+          src={imgUrl}
+          alt=''
+          className='absolute top-0 left-0 w-full h-full object-cover duration-500 transition-transform group-hover:scale-[105%]'
+          loading='lazy'
+        />
+        <div className='absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-out bg-gray opacity-0 group-hover/overlay:opacity-40 hidden md:block' />
+
+        <div className='absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-out opacity-0 group-hover/overlay:opacity-100 justify-center items-center hidden md:flex'>
+          <div className='flex flex-col gap-y-3'>
+            {heroLinks.map((heroLink) => (
+              <LinkCustom
+                key={heroLink.text}
+                to={heroLink.url}
+                className='z-30 py-[10px] bg-white hover:bg-gray border-none duration-100 block relative w-full'
+              >
+                {heroLink.text}
+              </LinkCustom>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='px-6 py-3 text-gray'>
+        <h4 className='capitalize border-b-[1px] border-b-gray-light font-bold pb-[10px] mb-[10px] text-[14px] tracking-wide'>
+          {title}
+        </h4>
+        <div className='flex gap-x-2 items-center mb-2'>
+          <img src={miniImgUrl} alt='' className='w-6' />
+          <p className='leading-[1.4] text-[12px] tracking-[0.3px]'>
+            {description}
+          </p>
+        </div>
+        <div className='flex md:hidden flex-col gap-y-2'>
+          {heroLinks.map((heroLink) => (
+            <LinkCustom
+              key={heroLink.text}
+              to={heroLink.url}
+              className='w-full p-2 text-[9px]'
+            >
+              {heroLink.text}
+            </LinkCustom>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GiftCard;
